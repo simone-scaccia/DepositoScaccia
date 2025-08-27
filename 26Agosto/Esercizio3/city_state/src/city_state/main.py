1731b0d2d1f5fd5efa94bcea7bdb7618f3cf029b
simone-scaccia <simone.scaccia@it.ey.com>
2025-08-27 10:49:25 +0200

    feat(city_country): add flow city - country

--- 26Agosto/Esercizio3/city_state/src/city_state/main.py
+++ 26Agosto/Esercizio3/city_state/src/city_state/main.py
@@ -1,12 +1,12 @@
-from crewai.flow.flow import Flow, listen, start
+from crewai.flow.flow import Flow, listen, start, router
 from dotenv import load_dotenv
 from litellm import completion
 
 class ExampleFlow(Flow):
-    model = "gpt-4o-mini"
+    model = "azure/gpt-4o"
 
     @start()
-    def generate_city(self):
+    def generate_city_or_country(self):
         print("Starting flow")
         print(f"Flow State ID: {self.state['id']}")  # ID univoco della run
 
@@ -15,20 +15,45 @@ class ExampleFlow(Flow):
             messages=[
                 {
                     "role": "user",
-                    "content": "Return the name of a random city in the world.",
+                    "content": "Return the name of a random city in the world OR a random country.",
                 },
             ],
         )
 
-        random_city = response["choices"][0]["message"]["content"]
+        random_city_or_country = response["choices"][0]["message"]["content"]
 
         # Salvo la città nello stato del flow
-        self.state["city"] = random_city
-        print(f"Random City: {random_city}")
-        return random_city
+        self.state["city_or_country"] = random_city_or_country
+        print(f"Random City or Country: {random_city_or_country}")
+        return random_city_or_country
 
-    @listen(generate_city)
-    def generate_fun_fact(self, random_city):
+    @listen(generate_city_or_country)
+    def discriminator(self, random_city_or_country):
+        response = completion(
+            model=self.model,
+            messages=[
+                {
+                    "role": "user",
+                    "content": f"Is {random_city_or_country} a city or a country? 1 for city, 0 for country. Return only 0 or 1",
+                },
+            ],
+        )
+
+        print(f"Discriminator Response: {response['choices'][0]['message']['content']}")
+        is_city = response["choices"][0]["message"]["content"] == "1"
+        self.state["is_city"] = is_city
+        return is_city
+
+    @router(discriminator)
+    def route_discriminator(self, is_city):
+        if is_city:
+            return "city"
+        else:
+            return "country"
+
+    @listen("city")
+    def generate_fun_fact(self):
+        random_city = self.state.get("city_or_country")
         response = completion(
             model=self.model,
             messages=[
@@ -43,9 +68,32 @@ class ExampleFlow(Flow):
         self.state["fun_fact"] = fun_fact
         return fun_fact
 
-# Avvio del flow
-flow = ExampleFlow()
-flow.plot()      # Mostra il diagramma
-result = flow.kickoff()
+    @listen("country")
+    def generate_country_neighborhood(self):
+        random_country = self.state.get("city_or_country")
+        response = completion(
+            model=self.model,
+            messages=[
+                {
+                    "role": "user",
+                    "content": f"Tell me the name of neighbors of {random_country}.",
+                },
+            ],
+        )
 
-print(f"Generated fun fact: {result}")
+        neighborhood_fact = response["choices"][0]["message"]["content"]
+        self.state["neighborhood_fact"] = neighborhood_fact
+        return neighborhood_fact
+
+
+def kickoff():
+    result = ExampleFlow().kickoff()
+    print(f"{result}")
+    plot()
+
+def plot():
+    flow = ExampleFlow()
+    flow.plot()
+
+if __name__ == "__main__":
+    kickoff()
